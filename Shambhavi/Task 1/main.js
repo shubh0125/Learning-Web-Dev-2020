1da80945736d9b7384dfd57453d948b7d6bc6440
ShambhaviSharma0110 <60618802+ShambhaviSharma0110@users.noreply.github.com>
2020-04-09 16:12:40 +0530

    Add files via upload

---
+++ Shambhavi/Task 1/main.js
@@ -0,0 +1,54 @@
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var question5 = document.quiz.question5.value;
+    var correct = 0;
+
+
+	if (question1 == "Beyond The Scene") {
+		correct++;
+}
+	if (question2 == "7") {
+		correct++;
+}	
+	if (question3 == "2013") {
+		correct++;
+    }
+    if (question4 == "South Korea"){
+        correct++;
+    }	
+    if (question5 == "Billboard Music Award"){
+        correct++;
+    }
+	var messages = ["Great job!","Good Attempt!","Fair enough","Did you guess?", "You at uno bro!", "Google BTS ooof"];
+	var score;
+
+	if (correct == 0) {
+		score = 5;
+	}
+
+	else if (correct == 1) {
+		score = 4;
+    }
+    else if (correct == 2){
+        score = 3;
+    }
+    else if (correct == 3){
+        score = 2;
+    }
+    else if (correct == 4){
+        score = 1;
+    }
+	else {
+		score = 0;
+    }
+
+
+	document.getElementById("after_submit").style.visibility = "visible";
+    document.getElementById("message").innerHTML = messages[score];
+    document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	}
+	
